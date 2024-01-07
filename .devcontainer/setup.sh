ad2871be1b05c715167d84c0affd2096e0c2f073
kumailr7 <kumailrizvi70@gamil.com>
2024-01-07 22:20:52 +0530

    Updating K3D Cluster Codespace

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,19 @@
 #!/bin/bash
 
 # Start k3d cluster with three nodes
-k3d cluster create mycluster --servers 3
+k3d cluster create Dragon --servers 2
+
+# Adding Nodes to the cluster 
+
+k3d node create Goku --cluster Dragon
+
+k3d node create Beast --cluster Dragon
 
 
 # Adding the config File
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Show the kubectl nodes 
+kubectl get nodes 
