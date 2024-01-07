58918f575c7be6c6a447a274ce44cfcff46d472e
kumailr7 <kumailrizvi70@gamil.com>
2024-01-07 21:57:34 +0530

    Updating K3D Cluster Codespace

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,3 +3,9 @@
 
 # Start k3d cluster with three nodes
 k3d cluster create mycluster --servers 3
+
+
+# Adding the config File
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
